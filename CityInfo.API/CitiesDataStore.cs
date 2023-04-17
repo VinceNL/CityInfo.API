a8e088486d1c318f7c9a9d94fd84a7fdeedeb7d6
vvancampen <vvancampen@msn.com>
2023-04-17 23:30:24 +0200

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,80 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            // init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Amsterdam",
+                    Description = "The one with the windows",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Vondelpark",
+                            Description = "Stads park"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "De Dam",
+                            Description = "Hart van de stad"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Nijmegen",
+                    Description = "Home sweet home",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Kronenburger park",
+                            Description = "Stads park"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Waalkade",
+                            Description = "Stad aan de Waal"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Arnhem",
+                    Description = "Why do I feel uncomfortable?",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Burgers Zoo",
+                            Description = "Dierentuin"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "Kranenburg",
+                            Description = "Stads wijk"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "A valid name value should be provided")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+    }
+}
